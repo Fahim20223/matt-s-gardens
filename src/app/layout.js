1223b9b0d69c199d8abaa3490161ca15ed18962f
Fahim2023 <fahimshahrier2023@gmail.com>
2026-04-03 23:05:25 +0600

    fixed linked favicon with the project logo

--- src/app/layout.js
+++ src/app/layout.js
@@ -15,6 +15,11 @@ const geistMono = Geist_Mono({
 export const metadata = {
   title: "Matt's Gardens",
   description: "Professional gardening services in Hertfordshire",
+  icons: {
+    icon: "/favicon.svg",
+    shortcut: "/favicon.svg",
+    apple: "/favicon.svg",
+  },
 };
 
 export default function RootLayout({ children }) {
